d71c5d088a21e8f8061a32528e5f1f53199f5361
meryamgh <113671198+meryamgh@users.noreply.github.com>
2023-09-17 22:05:18 +0200

    books component create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+import { FirstpageComponent } from './firstpage/firstpage.component';
+import { LastpageComponent } from './lastpage/lastpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    FirstpageComponent,
+    LastpageComponent
   ],
   imports: [
     BrowserModule,
